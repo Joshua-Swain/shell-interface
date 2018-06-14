2dba29cb6d7ec23c7e9b6cf65ea6d7d5b6867bff
Joshua Swain <Joshua.Swain@dal.ca>
2018-06-14 00:49:37 -0300

    Executes basic bash commands

--- assignment1.c
+++ assignment1.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <ctype.h>
 
 #define MAX_LINE 80 /* The maximum length command */
 
 void readline(char * args []);
+int argc;
 
 int main(void)
 {
@@ -16,50 +21,47 @@ int main(void)
   while (should_run){
     printf("CSCI3120>");
     fflush(stdout);
-    // This is a good spot to free the mem allocated by malloc in readline(char*[])
-    /*
-    for (i=0; i<(sizeof(args)/8); i++) {
-      if (args[i] == '\0') {
-        break;
-      }
-      free(args[i]);
-    }
-    */
-    memset(args, '\0', sizeof(args));
-    
-    readline(args); 
 
-    // Print the args array
-    for (i=0; i<(sizeof(args)/8); i++) {
-      if (args[i] == '\0') {
-        break;
-      }
-      printf("%s | ", args[i]);
-    }
-    printf("\n");
-    
-    /**
-    * Take m input and keep in list/array with number
-    * Parse input -> error handling
-    * Look for "&" in the input
+    memset(args, '\0', sizeof(args));
+
+    readline(args);
+
+    int background = 0; //for executing in the background
+
+    /*
     *
-    *
-    * 1. After reading input, fork child process using fork() -> must run concurrently
-    * 2. Child process will invoke execvp()
-    * 3. If command included "&", parent will invoke wait()
-    *
-    *
-    * Then:
-    * Make history()
-    * Make !! function
-    * Make !N function
+    * After reading user input, the steps are:
+    * (1) fork a child process using fork()
+    * (2) the child process will invoke execvp()
+    * (3) if command included &, parent will invoke wait()
     */
+
+    //forks a child process
+   	pid_t pid = fork();
+   	if (pid < 0)
+   	{
+      fprintf(stderr, "Fork Failed!");
+   	}
+   	else if (pid > 0) // parent process
+   	{
+      if (!background) //if not background, wait's until child is done
+     	{
+        wait(NULL);
+     	}
+   	}
+   	else // else child executes command
+   	{
+      if (execvp(args[0], args) < 0)
+     	{
+        exit(errno);
+     	}
+   	}
   }
 }
 
 /*
  * Reads a command and returns it as an array of strings
- */ 
+ */
 void readline(char * args[]) {
   // read line and store as char array
   int i = 0;
@@ -69,35 +71,29 @@ void readline(char * args[]) {
   int arg_count = 0;
   char line[640];
   char *cmd = fgets(line, ((MAX_LINE/2)+1)*8, stdin);
-    
-  if (!cmd) { // this if condition is only for testing purposes. May serve as error handeling later.
-    printf("Line not read.\n");
-  }
-  else {
-    printf("Line was read.\n");
-     
+
+
     while (!no_more_args) {
       // Get length of next word, store word in char []
       word_length = 0;
       while (!(isspace(cmd[cursor++]))) {
         word_length++;
       }
-        
+
       char word [word_length + 1];
       for (i=0; i<word_length; i++) {
         word[i] = cmd[cursor - word_length - 1 + i];
       }
       word[word_length] = '\0';
-      
+
       // Put word into args[]
       if (word_length > 0) {
         args[arg_count] = (char *) malloc(word_length + 1);
         strcpy(args[arg_count++], word);
       }
-      
+
       if (cmd[cursor-1] == '\n') {
         no_more_args = 1;
       }
-    }
   }
 }
